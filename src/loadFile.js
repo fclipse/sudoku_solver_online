c59aaaeb280c849c202a217ca53eb3c5e8617ffa
siejwkaodj <june123000@naver.com>
2023-12-07 14:21:32 +0900

    EDIT, ADD | cell초기화추가, 데이터 가공 코드 추가

--- src/loadFile.js
+++ src/loadFile.js
@@ -1,8 +1,13 @@
 // input 요소 가져오기
 const fileInput = document.getElementById('fileInput');
 
-// input 요소에 change 이벤트 리스너 등록
+// 파일 선택시 eventlistener
 fileInput.addEventListener('change', function(event) {
+    // cell 초기화
+    cellList.forEach((cell) => {
+        cell.value = ``;
+    });
+
     // 적절한 파일이 아닐 경우 return
     if(!validFileType(event.target.files[0])){
         alert('형식에 맞지 않는 파일입니다.');
@@ -31,12 +36,22 @@ function parseCSV(csvData) {
     // CSV 데이터를 줄 단위로 분할
     const cellList = document.querySelectorAll('.cell');
     const lines = csvData.split('\n');
+    // console.log(csvData);
+    // console.log(lines);
 
     // 읽어온 데이터를 input에 저장
     let idx = 0;
     lines.forEach(function(line) {
         line.split(',').forEach((col)=>{
-            cellList[idx++].value = col;
+            col = col.replace(/\r/g, '');   // '\r' 문자 제거
+            col = parseInt(col);
+            if(typeof col === 'number' && 1 <= col && col <= 9){
+                cellList[idx++].value = col;
+            }else if(col === 0){
+                idx++;
+            }else{
+                console.error(`${typeof col === 'string'}, "${col}", ${1 <= parseInt(col)}, ${parseInt(col) <= 9}`);
+            }
         })
         
     });
